57c4a31414a23ce1302d287ad2668843d87c607b
iram garcia ortega <iramgarcia97@gmail.com>
2019-09-22 21:21:10 -0700

    botones

---
+++ Assets/Scripts/PauseTimeline.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class PauseTimeline : MonoBehaviour
+{
+    Button btnPauseTimeline;
+
+    static bool pause = false;
+
+    [SerializeField]
+    PlayableDirector playableDirector;
+
+    void Awake()
+    {
+      
+        btnPauseTimeline = GetComponent<Button>();
+        btnPauseTimeline.onClick.AddListener(ButtonPauseTimeline);
+
+
+    }
+
+
+
+    void ButtonPauseTimeline()
+    {
+
+        playableDirector.Pause();
+        pause = true;
+    }
+
+ 
+
+}
---
+++ Assets/Scripts/StopTimeline.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class StopTimeline : MonoBehaviour
+{
+
+    Button btnStopTimeline;
+
+    [SerializeField]
+    PlayableDirector playableDirector;
+
+    void Awake()
+    {
+    
+
+        btnStopTimeline = GetComponent<Button>();
+        btnStopTimeline.onClick.AddListener(ButtonStopTimeline);
+
+    }
+
+
+
+
+    void ButtonStopTimeline()
+    {
+        playableDirector.Stop();
+    }
+
+}
